15dca1172a1829d8c6f92070a0b8f1702dff3fb0
BurakHINGE <burakmentese16@gmail.com>
2026-08-21 21:30:13 +0300

    Create Library
    Library has been created.

--- Codes/Library.java
+++ Codes/Library.java
@@ -2,21 +2,101 @@ import java.util.ArrayList;
 
 public class Library {
 
-    public static final int SHORT = 0;
-    public static final int ALL_REQ_DENIED = 1;
-    public static final int CAPITAL_LOWER = 2;
-    public static final int CAPITAL_SPECIAL = 3;
-    public static final int LOWER_SPECIAL = 4;
-    public static final int SPECIAL = 5;
-    public static final int LOWER = 6;
-    public static final int CAPITAL = 7;
-    public static final int ALLOWED = 8;
+    private ArrayList<LibraryBook> books;
     
     public Library() {
-        
+
     }
 
-    
+    public boolean addBook(LibraryBook book) {
 
+        for (LibraryBook tempBook : books) {
+            if (tempBook.getBook().getID() == book.getBook().getID()) {
+                return false;
+            }
+        }
+
+        books.add(book);
+        return true;
+    }
+
+    public boolean removeBook(LibraryBook book) {
+
+        for (LibraryBook tempBook : books) {
+            if (tempBook.getBook().getID() == book.getBook().getID()) {
+                books.remove(book);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public LibraryBook findBook(int id) {
+
+        for (LibraryBook tempBook : books) {
+            if (tempBook.getBook().getID() == id) {
+                return tempBook;
+            }
+        }
+
+        return books.get(id);
+    }
+
+    public boolean borrowBookFromLibrary(int bookID, User user) {
+
+        LibraryBook libraryBook = findBook(bookID);
+
+        if (libraryBook == null) {
+            return false;
+        }
+
+        if (!libraryBook.borrow()) {
+            return false;
+        }
+
+        ReadingRecord record = new ReadingRecord(libraryBook.getBook(), BookSource.LIBRARY);
+        record.setStatus(ReadingStatus.READING);
+
+        if (!user.addBooks(record)) {
+            libraryBook.returnBook();
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean returnBookToLibrary(int bookID, User user) {
+
+        LibraryBook libraryBook = findBook(bookID);
+
+        if (libraryBook == null) {
+            return false;
+        }
+
+        ReadingRecord targetRecord = null;
+
+        for (ReadingRecord record : user.getBooks()) {
+            if (record.getBook().getID() == bookID && record.getSource() == BookSource.LIBRARY) {
+                targetRecord = record;
+                break;
+            }
+        }
+
+        if (targetRecord == null) {
+            return false;
+        }
+
+        if (!libraryBook.returnBook()) {
+            return false;
+        }
+
+        user.removeBooks(targetRecord);
+        return true;
+    }
+    
+    public ArrayList<LibraryBook> getBooks() {
+        return books;
+    }
 
 }
