25b11da9d9ac5769408a768ffa2a11596acbac0b
BurakHINGE <burakmentese16@gmail.com>
2026-08-23 04:10:40 +0300

    Update Main Class

--- Codes/MainLibrary.java
+++ Codes/MainLibrary.java
@@ -4,8 +4,9 @@ public class MainLibrary {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        Library library = new Library();
         AuthenticationManager auth = new AuthenticationManager();
-        UserMenu userMenu = new UserMenu(auth);
+        UserMenu userMenu = new UserMenu(library, auth);
         MenuManager mainMenu = new MenuManager(auth, userMenu);
 
         mainMenu.showMainMenu();
