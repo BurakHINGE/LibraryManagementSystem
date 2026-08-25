73a35baa67358c62358fe3f9d210c5eb408875e3
BurakHINGE <burakmentese16@gmail.com>
2026-08-25 17:15:18 +0300

    Update Loan

--- Codes/Loan.java
+++ Codes/Loan.java
@@ -4,15 +4,13 @@ import java.time.LocalDate;
 public class Loan {
     
     private int loanID;
-    private User user;
     private LibraryBook libraryBook;
     private LocalDate borrowDate;
     private LocalDate returnDate;
     private boolean returned;
 
-    public Loan(int loanID, User user, LibraryBook libraryBook) {
+    public Loan(int loanID, LibraryBook libraryBook) {
         this.loanID = loanID;
-        this.user = user;
         this.libraryBook = libraryBook;
         this.borrowDate = null;
         this.returnDate = null;
@@ -45,10 +43,6 @@ public class Loan {
 
     public int getLoanID() {
         return loanID;
-    }
-
-    public User getUser() {
-        return user;
     }
 
     public LibraryBook getLibraryBook() {
